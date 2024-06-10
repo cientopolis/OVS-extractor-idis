eb9f77cf678420a60131d7de73b38de13be579a7
Mateo Ibanez <mateoiba30@gmail.com>
2024-06-09 21:48:47 -0300

    agrego patron de preventa y modifico DB

--- extractor/src/helper.py
+++ extractor/src/helper.py
@@ -1,12 +1,51 @@
 import re
-
 import spacy
+from spacy.tokens import Doc, Span
+import datetime
 
 NLP = spacy.load("es_core_news_lg")
 
 RE_DOS = re.compile(r"\b(dos|doble|2|segundo)\b", re.IGNORECASE)
 RE_TRES = re.compile(r"\b(tres|triple|3|tercer)\b", re.IGNORECASE)
 
+def procesar_preventa(predichos: list):
+    matcheos = 0
+    minimosMatcheos = 2
+    maximoAniosLejanos = 15
+
+    for match_id, start, end in predichos["asegurados"]:
+        matcheos += minimosMatcheos + 1
+
+    matcheosAuxiliar = 0
+    for match_id, start, end in predichos["posibles"]:
+        matcheosAuxiliar += 1
+    if matcheosAuxiliar > 0:
+        matcheos += 1
+
+    matcheosAuxiliar = 0
+    for match_id, start, end in predichos["fecha"]:
+        span = doc[start:end]
+        span.text.replace('.', '') #para que no haya problemas con los puntos -> no se si me está dando efecto
+        try:
+            num = float(span.text)
+            actualYear = datetime.datetime.now().year
+            if (num > actualYear) and (num < actualYear + maximoAniosLejanos):
+                matcheosAuxiliar +=1
+        except ValueError:
+            pass
+    if matcheosAuxiliar > 0:
+        matcheos += 1
+
+    matcheosAuxiliar = 0
+    for match_id, start, end in predichos["cuotas"]:
+        matcheosAuxiliar += 1
+    if matcheosAuxiliar > 0:
+        matcheos += 1
+
+    for match_id, start, end in predichos["descartar"]:
+        matcheos = 0
+
+    return (matcheos >= minimosMatcheos)
 
 def clear_altura_entre(predichos: list):
     if predichos["dir_nro"] and predichos["dir_entre"]:
@@ -108,16 +147,54 @@ def procesar_frentes(frentes_predichos):
     return max(frentes_en_numeros) if len(frentes_en_numeros) != 0 else ""
 
 
-def procesar_medidas(predichos: list):
-    mejor_match = max(predichos, key=len)
+def procesar_medidas(predichos_todos: list):
+    """
+    Si el aviso enuncia múltiples dimensiones, buscar cuál refiere al lote
+    """
+    mejor_match= max(predichos_todos, key=len) if predichos_todos else ""
+    
     if "martillo" in mejor_match:
         return mejor_match.replace(" mts", "")
 
     medidas = ""
     for numero in list(map(str, get_numeros(mejor_match))):
         medidas += numero + " x "
-
+        
+    medidas= medidas.replace(",",".")
     return medidas.rstrip(" x")
+
+def procesar_medidas_multi(predichos_todos: list):
+    """
+    Si el aviso es multioferta, devolver todas las direcciones
+    """
+    result= []
+    for candidato in list(__reduce_superstrings(set(predichos_todos))):
+        medidas = ""
+        for numero in list(map(str, get_numeros(candidato))):
+            medidas += numero + " x "
+        
+        if (medidas.count("x")>1):
+            medidas= medidas.replace(",",".")
+            result.append(medidas.rstrip(" x"))
+
+
+    if len(result)>1:
+        return ";".join(result)
+    return "".join(result)
+
+
+
+def __reduce_superstrings(dimensions):
+    reduced_dimensions = []
+    for dim in dimensions:
+        # Si la dimensión actual no es un superstring de ninguna dimensión ya incluida
+        if not any(dim in existing or existing in dim for existing in reduced_dimensions):
+            # Eliminar superstrings de la lista de resultados
+            reduced_dimensions = [existing for existing in reduced_dimensions if dim not in existing and existing not in dim]
+            # Agregar la dimensión actual a la lista de resultados
+            reduced_dimensions.append(dim)
+    return reduced_dimensions
+
 
 
 def procesar_barrio(predichos: list):
@@ -136,3 +213,6 @@ def descubrir_nuevos(predichos: dict):
         predichos["frentes"] = 2.0
 
     return predichos
+
+def es_multioferta(predichos: list):
+    return True if predichos else ""
