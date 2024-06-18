86798b9f89812a53a9c2b0dde8ae87848016758b
Mateo Ibanez <mateoiba30@gmail.com>
2024-06-18 12:16:01 -0300

    f1 de 097

--- extractor/src/rbm/patterns/a_demoler.py
+++ extractor/src/rbm/patterns/a_demoler.py
@@ -1,4 +1,15 @@
+idealSinonimos = ["para", "ideal", "posibilidad"]
+ideales  = ["edificar", "edificio", "edificios", "constructor","constructores", "construccion", "construcciones", "construcción", "inversor", "inversores", "emprendimiento", "emprendimientos", "desarrollo", "desarrollos"]
+demolerSinonimos = ["reciclar", "refaccionar", "demoler", "demolicion", "demolición"]
+casaSinonimos= ["casa", "construccion", "construcción"] #PH
+antiguaSinonimos = ["antigua", "precaria"] #vieja
+
 def a_demoler()->list:
     return  list([
-        [{"LOWER": "demoler"}],
+        #a demoler
+        [{"LOWER": {"IN": demolerSinonimos}}],
+        [{"LOWER": {"IN": casaSinonimos}},{"LOWER": {"IN": antiguaSinonimos}}],
+
+        #bueno para construir
+        [{"LOWER": {"IN": idealSinonimos}}, {"TEXT": "de", "OP":"?"}, {"LOWER": {"IN": ideales}}],
         ])
