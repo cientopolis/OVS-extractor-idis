4f24654523e75fea4fae83a889eeae26068f1f65
Mateo Ibanez <mateoiba30@gmail.com>
2024-06-18 13:22:47 -0300

    corrijo campos de la DB

--- extractor/src/rbm/patterns/a_demoler.py
+++ extractor/src/rbm/patterns/a_demoler.py
@@ -1,5 +1,5 @@
 idealSinonimos = ["para", "ideal", "posibilidad"]
-ideales  = ["edificar", "edificio", "edificios", "constructor","constructores", "construccion", "construcciones", "construcción", "inversor", "inversores", "emprendimiento", "emprendimientos", "desarrollo", "desarrollos"]
+ideales  = ["inversión", "inversion", "edificar", "edificio", "edificios", "constructor","constructores", "construccion", "construcciones", "construcción", "inversor", "inversores", "emprendimiento", "emprendimientos", "desarrollo", "desarrollos"]
 demolerSinonimos = ["reciclar", "refaccionar", "demoler", "demolicion", "demolición"]
 casaSinonimos= ["casa", "construccion", "construcción"] #PH
 antiguaSinonimos = ["antigua", "precaria"] #vieja
@@ -11,5 +11,5 @@ def a_demoler()->list:
         [{"LOWER": {"IN": casaSinonimos}},{"LOWER": {"IN": antiguaSinonimos}}],
 
         #bueno para construir
-        [{"LOWER": {"IN": idealSinonimos}}, {"TEXT": "de", "OP":"?"}, {"LOWER": {"IN": ideales}}],
+        [{"LOWER": {"IN": idealSinonimos}}, {"TEXT": "de", "OP":"?"}, {"LOWER": {"IN": ideales}}], #esto lo tengo que sacar?
         ])
