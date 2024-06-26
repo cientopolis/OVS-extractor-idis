ca3c950b60107ba5ec960638a5458167b675be88
Mateo Ibanez <mateoiba30@gmail.com>
2024-06-26 14:19:05 -0300

    hago cambios en patrón edificacion_monetizable

--- extractor/src/rbm/patterns/edificacion_monetizable.py
+++ extractor/src/rbm/patterns/edificacion_monetizable.py
@@ -1,4 +1,21 @@
+CON_SINONIMOS = ["c/", "con", "tiene", "posee"]
+UN_SINONIMOS = ["un", "una", "unos", "unas"]
+CONSTRUCCION_SINONIMOS = ["construcción", "construccion", "construcciones", "zócalo", "zocalo", "zocalos", "mejora", "mejoras", "edificación", "edificacion", "edificaciones", "quincho", "quinchos", "pileta", "piletas", "galpon", "galpón", "galpones", "local", "locales", "casa", "casas", "casita", "casitas", "paredón", "paredon", "paredones", "portón", "porton", "portones", "comedor", "comedores", "cocina", "cocinas", "cerco", "cercos", "habitación", "habitacion", "habitaciones", "alambrado", "alambrados", "parrilla", "parrillas", "plataforma", "plataformas", "loza", "lozas", "contrapiso", "contrapisos", "medianera", "medianeras", "garaje", "garajes"]
+SUPERFICIE_SINONIMOS = ["superficie", "espacio", "sup"]
+CUBIERTA_SINONIMOS = ["cubierta", "cubierto", "cubiertas", "cubiertos", "semicubierta", "semicubierto", "semicubiertas", "semicubiertos"]
+BASE_SINONIMOS = ["base", "bases"]
+AREA = ["m2", "mts2", "mt2", "metros2", "metro2"] #fijarme sk matchea con M2 y similares
+PALABRAS_CLAVE = ["parquizado", "parquizada", "nivelado", "nivelada"]
+
 def edificacion_monetizable():
     return  list([
-        [{"LIKE_NUM": True}],
+        #con construcción
+        [{"LOWER": {"IN":CON_SINONIMOS}},{"LOWER": {"IN":UN_SINONIMOS},"OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        [{"LOWER": {"IN":SUPERFICIE_SINONIMOS}},{"LOWER": "semi"},{"LOWER":"cubierta"}],
+        [{"LOWER": {"IN":SUPERFICIE_SINONIMOS}},{"LOWER": {"IN":CUBIERTA_SINONIMOS}}],    
+        [{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"LOWER": "metros"},{"LOWER": "cuadrados"}],
+        [{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"LOWER": {"IN":AREA}}],
+        [{"LOWER": {"IN":PALABRAS_CLAVE}}],
+
+        #si la construcción es futura entonces no ponerla
     ])
