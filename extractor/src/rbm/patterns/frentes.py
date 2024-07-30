477060eacb8356b681c1183c8d26de481d4d5fec
AlvaroPaganoLIFIA <paganoalvaroangel@gmail.com>
2024-07-30 15:14:21 -0300

    frentes : (f1=0.55 => f1=0.84)

--- extractor/src/rbm/patterns/frentes.py
+++ extractor/src/rbm/patterns/frentes.py
@@ -28,6 +28,30 @@ def frentes() -> list :
                 "RIGHT_ATTRS": {"DEP": "nummod"},
             }
         ],
+        [
+            {
+                "RIGHT_ID": "calles",
+                "RIGHT_ATTRS": {"LOWER": {"IN": ["calle", "calles","avenida","avenidas"]}},
+            },
+            {
+                "LEFT_ID": "calles",
+                "REL_OP": "<",
+                "RIGHT_ID": "acceso",
+                "RIGHT_ATTRS": {"POS": "NOUN"},
+            },
+            {
+                "LEFT_ID": "calles",
+                "REL_OP": ">",
+                "RIGHT_ID": "por",
+                "RIGHT_ATTRS": {"POS": "ADP"}
+            },
+            {
+                "LEFT_ID": "calles",
+                "REL_OP": ">",
+                "RIGHT_ID": "numero",
+                "RIGHT_ATTRS": {"POS": "NUM"}
+            }
+        ],
     ] 
 )
    
