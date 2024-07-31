bf13754d7db03a79880e7c4deeb010db154d9640
AlvaroPaganoLIFIA <paganoalvaroangel@gmail.com>
2024-07-31 11:45:30 -0300

    urb_cerrada : (f1 = 0.79  => f1 = 0.89)

--- extractor/src/rbm/patterns/frentes.py
+++ extractor/src/rbm/patterns/frentes.py
@@ -2,16 +2,16 @@ def frentes() -> list :
     return list (
     [
         [
-        {
-            "RIGHT_ID": "frentes",
-            "RIGHT_ATTRS": {"LOWER": {"IN": ["frente", "frentes","Frentes"]}},
-        },
-        {
-            "LEFT_ID": "frentes",
-            "REL_OP": ">",
-            "RIGHT_ID": "num",
-            "RIGHT_ATTRS": {"DEP": {"IN": ["nummod", "amod"]}},
-        }
+            {
+                "RIGHT_ID": "frentes",
+                "RIGHT_ATTRS": {"LOWER": {"IN": ["frente", "frentes","Frentes"]}},
+            },
+            {
+                "LEFT_ID": "frentes",
+                "REL_OP": ">",
+                "RIGHT_ID": "num",
+                "RIGHT_ATTRS": {"DEP": {"IN": ["nummod", "amod"]}},
+            }
         ],
         [
             {"RIGHT_ID": "frentes", "RIGHT_ATTRS": {"LOWER": "salida"}},
--- extractor/src/rbm/patterns/urb_cerrada.py
+++ extractor/src/rbm/patterns/urb_cerrada.py
@@ -2,27 +2,12 @@
 def urb_cerrada()-> list:
     return list(
         [
-            # [ 
-            #     {"LOWER": {"IN": ["barrio privado", "barrio cerrado", "club de campo","urbanización privada","barrio boutique","country","countries"]}},{"POS":"PROPN", "OP":"*"}
-            # ], no agarra bien los tokens entonces no matchea
-            # [{"LOWER": "barrio"}, {"LOWER": "privado"}],
-            # [{"LOWER": "barrio"}, {"LOWER": "cerrado"}],
-            # [{"LOWER": "club"}, {"LOWER": "de"}, {"LOWER": "campo"}],
-            # [{"LOWER": "urbanización"}, {"LOWER": "privada"}],
-            # [{"LOWER": "barrio"}, {"LOWER": "boutique"}],
-            # [{"LOWER": "country"}],
-            # [{"LOWER": "countries"}],
-            # [
-            #     {"LEMMA": {"IN": ["cerrado", "privado"]}}
-            # ],
-            # [
-            #     {"LOWER": "lote"},{"IS_DIGIT": True}
-            # ], baja la presicion porque matchea muchos que no so
-            # [
-            #     {"LOWER": "lote"},{"TEXT":{"REGEX": "[A-Za-z]\d+"}}
-            # ],por si solo no es indicativo de que sea barrio privado entonces resta presicion
-            #[{"LOWER": "seguridad"}],
-            #[{"LOWER": "amenities"}, {"LOWER": "generales", "OP":"?"}],
-            #[{"LOWER": "cancha"}, {"LOWER": "de"}, {"LOWER": {"REGEX": ".*"}}]
+            [{"LOWER" : "barrio"},{"LOWER" : "cerrado"}],
+            [{"LOWER" : "barrios"},{"LOWER" : "cerrados"}],
+            [{"LOWER" : "club"},{"LOWER" : "house"}],
+            [{"LOWER" : "fincas"},{"LOWER" : "de"}],
+            [{"LOWER" : "condominio"},{"LOWER" : "cerrado"}],
         ]
     )
+
+# relizar dependency matcher para descartar los casos como: "lindero a club de campo Grand Bell"
