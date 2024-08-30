14743ad23b905c3568f95a96e90fb8e6f948b6f4
Mateo Ibanez <mateoiba30@gmail.com>
2024-08-30 12:55:09 -0300

    matcheo "con bases de construccion"

--- extractor/src/rbm/patterns/edificacion_monetizable.py
+++ extractor/src/rbm/patterns/edificacion_monetizable.py
@@ -1,26 +1,26 @@
 CON_SINONIMOS = ["c/", "con", "tiene", "posee", "hay"]
 UN_SINONIMOS = ["un", "una", "unos", "unas"]
 CONSTRUCCION_SINONIMOS = [ "vivienda", "viviendas", "depto", "deptos", "departamento", "departamentos", "edificado", "edificados", "deposito", "depósito", "depositos", "depósitos", "construcción", "construccion", "construcciones", "zócalo", "zocalo", "zocalos", "edificación", "edificacion", "edificaciones", "quincho", "quinchos", "parrilla", "parrillas", "galpon", "galpón", "galpones", "local", "locales", "casa", "casas", "casita", "casitas", "paredón", "paredon", "paredones", "comedor", "comedores", "cocina", "cocinas", "habitación", "habitacion", "habitaciones", "parrilla", "parrillas", "garaje", "garajes", "cabaña", "cabañas", "chalet", "chalets", "dormitorio", "dormitorios", "cochera", "cocheras", "ventana", "ventanas", "living", "livings", "lavadero", "lavaderos", "guardacoche", "guardacoches", "piso", "pisos", "muro", "muros", "hall", "halls", "terraza", "terrazas", "garage", "garages", "cocina", "cocinas", "lavadero", "lavaderos", "balcón", "balcon", "balcones", "comedor", "comedores", "galería", "galeria", "galerías", "galerias", "playroom", "playrooms", "local", "locales", "pórtico", "póritcos", "portico", "porticos", "porche", "porches"]
-SUPERFICIE_SINONIMOS = ["superficie", "espacio", "sup"]
+SUPERFICIE_SINONIMOS = ["superficie", "espacio", "sup", "Sup", "Sup."] #PROBAR AGREGAR ESTO
 CUBIERTA_SINONIMOS = ["edificada", "edificadas", "edificado", "edificados", "cubierta", "cubierto", "cubiertas", "cubiertos", "semicubierta", "semicubierto", "semicubiertas", "semicubiertos"]
-BASE_SINONIMOS = ["base", "bases"]
+BASE_SINONIMOS = ["base", "bases", "Base", "Bases"]
 AREA = ["m2", "mts2", "mt2", "metros2", "metro2"] #fijarme sk matchea con M2 y similares
 PALABRAS_CLAVE_EDIFICADO = ["parquizado", "parquizada", "nivelado", "nivelada"]
 CONSTRUIDO_SINONIMOS = ["construido", "construida", "construidos", "construidas", "creada", "creado", "creadas", "creados"]
 COSAS_COUNTRY = ["portón", "porton", "portones", "alambrado", "alambrada", "cercado", "cercados", "pileta", "piscina", "cerco", "cercos"]
 PALABRAS_CLAVE_MEJORADO = ["mejora", "mejoras", "mejorado", "mejorada","paredón", "paredon", "paredones", "contrapiso", "contrapisos", "medianera", "medianeras", "platea", "plateas", "loza", "lozas",]
 #"portón", "porton", "portones", 
-
+#EXTRA = ["paddle", "tennis", "tenis", "cancha", "canchas", "gimnasio", "gimnasios", "spa", "spas"]
 def construccion():
     return  list([
         # #con construcción
-        # [{"LOWER": "consta"},{"LOWER": "de"},{"POS":"ADV", "OP":"?"},{"LOWER": {"IN":UN_SINONIMOS},"OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
-        # [{"LOWER": {"IN":CON_SINONIMOS}},{"POS":"ADV", "OP":"?"},{"LOWER": {"IN":UN_SINONIMOS},"OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
-        # [{"LOWER": {"IN":CON_SINONIMOS}},{"POS":"ADV", "OP":"?"},{"POS": "DET"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
-        # [{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}},{"LOWER":{"IN":CONSTRUIDO_SINONIMOS}}],
+        [{"LOWER": "consta"},{"LOWER": "de"},{"POS":"ADV", "OP":"?"},{"LOWER": {"IN":UN_SINONIMOS},"OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        [{"LOWER": {"IN":CON_SINONIMOS}},{"POS":"ADV", "OP":"?"},{"LOWER": {"IN":UN_SINONIMOS},"OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        [{"LOWER": {"IN":CON_SINONIMOS}},{"POS":"ADV", "OP":"?"},{"POS": "DET"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        [{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}},{"LOWER":{"IN":CONSTRUIDO_SINONIMOS}}],
 
         #contrucción -> tal vez sea todo lo que necesite si lo junto con el Dependency Matcher
-        [{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        #[{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
 
         #superficie cubierta
         [{"LOWER": {"IN":SUPERFICIE_SINONIMOS}},{"LOWER": "semi"},{"LOWER":"cubierta"}],
@@ -31,7 +31,8 @@ def construccion():
         #relacionado a Base
         [{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"LOWER": "metros"},{"LOWER": "cuadrados"}],
         [{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"LOWER": {"IN":AREA}}],
-        #[{"LOWER": {"IN":CON_SINONIMOS}},{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        [{"LOWER": {"IN":CON_SINONIMOS}},{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        #probar de descomentar esto de arriba????????????????????
 
         #palabras que no requieren "con"
         [{"LOWER": {"IN":PALABRAS_CLAVE_EDIFICADO}}],
