1c292277769a31f4fa1056959c4619110fe000ef
Mateo Ibanez <mateoiba30@gmail.com>
2024-08-30 13:59:52 -0300

    reordeno el es_monetizable

--- extractor/src/rbm/patterns/edificacion_monetizable.py
+++ extractor/src/rbm/patterns/edificacion_monetizable.py
@@ -12,9 +12,9 @@ PALABRAS_CLAVE_EDIFICADO_CON = ["mejora", "mejoras", "mejorado", "mejorada"]
 
 CON_SINONIMOS = ["c/", "con", "tiene", "posee", "hay"]
 UN_SINONIMOS = ["un", "una", "unos", "unas"]
-SUPERFICIE_SINONIMOS = ["superficie", "espacio", "sup", "Sup", "Sup."] #PROBAR AGREGAR ESTO
+SUPERFICIE_SINONIMOS = ["superficie", "espacio", "sup", "sup."]
 CUBIERTA_SINONIMOS = ["edificada", "edificadas", "edificado", "edificados", "cubierta", "cubierto", "cubiertas", "cubiertos", "semicubierta", "semicubierto", "semicubiertas", "semicubiertos"]
-BASE_SINONIMOS = ["base", "bases", "Base", "Bases"]
+BASE_SINONIMOS = ["base", "bases"]
 AREA = ["m2", "mts2", "mt2", "metros2", "metro2"] #fijarme sk matchea con M2 y similares
 CONSTRUIDO_SINONIMOS = ["construido", "construida", "construidos", "construidas", "creada", "creado", "creadas", "creados"]
 COSAS_COUNTRY = ["portón", "porton", "portones", "alambrado", "alambrada", "cercado", "cercados", "pileta", "piscina", "cerco", "cercos"]
@@ -22,14 +22,14 @@ COSAS_COUNTRY = ["portón", "porton", "portones", "alambrado", "alambrada", "cer
 #EXTRA = ["paddle", "tennis", "tenis", "cancha", "canchas", "gimnasio", "gimnasios", "spa", "spas"]
 def construccion():
     return  list([
-        # #con construcción
+        #con construcción
         [{"LOWER": "consta"},{"LOWER": "de"},{"POS":"ADV", "OP":"?"},{"LOWER": {"IN":UN_SINONIMOS},"OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
         [{"LOWER": {"IN":CON_SINONIMOS}},{"POS":"ADV", "OP":"?"},{"LOWER": {"IN":UN_SINONIMOS},"OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
         [{"LOWER": {"IN":CON_SINONIMOS}},{"POS":"ADV", "OP":"?"},{"POS": "DET"},{"LIKE_NUM":True,"OP":"?"},{"POS": "ADJ", "OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
         [{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}},{"LOWER":{"IN":CONSTRUIDO_SINONIMOS}}],
 
-        #contrucción -> tal vez sea todo lo que necesite si lo junto con el Dependency Matcher
-        #[{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
+        #palabras que no requieren "con"
+        [{"LOWER": {"IN":PALABRAS_CLAVE_EDIFICADO}}],
 
         #superficie cubierta
         [{"LOWER": {"IN":SUPERFICIE_SINONIMOS}},{"LOWER": "semi"},{"LOWER":"cubierta"}],
@@ -41,16 +41,15 @@ def construccion():
         [{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"LOWER": "metros"},{"LOWER": "cuadrados"}],
         [{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LIKE_NUM":True,"OP":"?"},{"LOWER": {"IN":AREA}}],
         [{"LOWER": {"IN":CON_SINONIMOS}},{"LOWER": {"IN":BASE_SINONIMOS}},{"LOWER":"de","OP":"?"},{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}}],
-
-        #palabras que no requieren "con"
-        [{"LOWER": {"IN":PALABRAS_CLAVE_EDIFICADO}}],
-        #si la construcción es futura entonces no ponerla
     ])
 
 
 def mejorado():
     return  list([
+        #mejora
         [{"LOWER": {"IN":PALABRAS_CLAVE_MEJORADO}}],
+
+        #con mejora
         [{"LOWER":{"IN":CONSTRUCCION_SINONIMOS}},{"LOWER":{"IN":PALABRAS_CLAVE_EDIFICADO_CON}}],
     ])
 
