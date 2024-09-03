20a0eb31837117d2b274545bbb658f103e331eec
Mateo Ibanez <mateoiba30@gmail.com>
2024-09-03 14:55:30 -0300

    corrijo db

--- extractor/src/rbm/patterns/pileta.py
+++ extractor/src/rbm/patterns/pileta.py
@@ -2,6 +2,7 @@ piletaSinonimos = ["pileta", "piscina"]
 antiguaSinonimos = ["antigua", "precaria"]
 demolerSinonimos = ["reciclar", "refaccionar", "demoler", "demolicion", "demolición"]
 conectores = ["para", "a"]#que necesita, se encuentra a , destinada para, ideal para (ya son muchas cosas posibles, conviene un DM)
+POSIBLE_COUNTRY = ["cancha", "canchas", "futbol", "fútbol", "tenis", "tennis", "rugby", "spa", "spas", "gimnasio", "gimnasios", "paddle" ]
 
 def pileta() -> list:
     return list ( 
@@ -17,7 +18,10 @@ def pileta_barrio() -> list:
             #dentro del barrio
             [{"LOWER" : {"IN" : ["piscinas","piletas"]}}], #dejarlo porque hay veces que lo dicen en plural y singular en la misma publicación
             [{"LOWER" : {"IN": ["pileta"]}},{"LOWER" : "climatizada"}], #poner , piscina"?
-            [{"LOWER" : {"IN": piletaSinonimos}},{"LOWER" : "semi-olímpica"}]
+            [{"LOWER" : {"IN": piletaSinonimos}},{"LOWER" : "semi-olímpica"}],
+
+            #posible country, aunque no lo sea no quiero matchear con la pileta
+            [{"LOWER" : {"IN": POSIBLE_COUNTRY}}]
 
             #pileta a refaccionar -> no mejora nada por ahora
             # [{"LOWER": {"IN": piletaSinonimos}},{"LOWER": {"IN":conectores}},{"LOWER": {"IN": demolerSinonimos}}],
