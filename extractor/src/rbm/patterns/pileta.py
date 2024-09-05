05608fccdae5b7c844105cd67e61e54919b683bb
Mateo Ibanez <mateoiba30@gmail.com>
2024-09-05 13:32:28 -0300

    mejoro patron de pileta

--- extractor/src/rbm/patterns/pileta.py
+++ extractor/src/rbm/patterns/pileta.py
@@ -2,7 +2,9 @@ piletaSinonimos = ["pileta", "piscina"]
 antiguaSinonimos = ["antigua", "precaria"]
 demolerSinonimos = ["reciclar", "refaccionar", "demoler", "demolicion", "demolición"]
 conectores = ["para", "a"]#que necesita, se encuentra a , destinada para, ideal para (ya son muchas cosas posibles, conviene un DM)
-POSIBLE_COUNTRY = ["cancha", "canchas", "futbol", "fútbol", "tenis", "tennis", "rugby", "spa", "spas", "gimnasio", "gimnasios", "paddle" ]
+POSIBLE_COUNTRY = ["cancha", "canchas", "futbol", "fútbol", "tenis", "tennis", "rugby", "spa", "spas", "gimnasio", "gimnasios", "paddle", "hockey", "polo"] #polo
+DE_SINONIMOS = ["de", "para"]
+NATACION_SINONIMOS = ["natacion", "natación"]
 
 def pileta() -> list:
     return list ( 
@@ -17,8 +19,12 @@ def pileta_barrio() -> list:
         [
             #dentro del barrio
             [{"LOWER" : {"IN" : ["piscinas","piletas"]}}], #dejarlo porque hay veces que lo dicen en plural y singular en la misma publicación
-            [{"LOWER" : {"IN": ["pileta"]}},{"LOWER" : "climatizada"}], #poner , piscina"?
+            #[{"LOWER" : {"IN": piletaSinonimos}},{"LOWER" : "climatizada"}], #baja rendimiento
+
+            #piletas grandes
             [{"LOWER" : {"IN": piletaSinonimos}},{"LOWER" : "semi-olímpica"}],
+            #[{"LOWER" : {"IN": piletaSinonimos}},{"LOWER" : {"IN": DE_SINONIMOS}},{"LOWER" : {"IN": NATACION_SINONIMOS}}],#pileta de natación
+            #gran pileta
 
             #posible country, aunque no lo sea no quiero matchear con la pileta
             [{"LOWER" : {"IN": POSIBLE_COUNTRY}}]
@@ -26,5 +32,7 @@ def pileta_barrio() -> list:
             #pileta a refaccionar -> no mejora nada por ahora
             # [{"LOWER": {"IN": piletaSinonimos}},{"LOWER": {"IN":conectores}},{"LOWER": {"IN": demolerSinonimos}}],
             # [{"LOWER": {"IN": piletaSinonimos}},{"LOWER": {"IN": antiguaSinonimos}}]
+            #pileta de lona
+            #con vista a piscina
         ]
     )
