06cda4d164e78aa18a5d505e6164450568a76831
AlvaroPaganoLIFIA <paganoalvaroangel@gmail.com>
2024-09-05 09:08:31 -0300

    elimino los 3 archivos csv y dejo el archivo normalizador listo para usar la funcion normalize

--- extractor/normalizador.py
+++ extractor/normalizador.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from normalize import normalize
 
-
 input_file =  '/home/apagano/OVS-extractor-idis/input/ground_truth_100_sin_inferencias.csv'
 output_file = '/home/apagano/OVS-extractor-idis/input/ground_truth_100_sin_inferencias_normalizado.csv'
 
-df = pd.read_csv(input_file, delimiter='|', dtype=str) #Leo el archivo CSV como un dataFrame
+df = pd.read_csv(input_file, delimiter='|', dtype=str,keep_default_na=False) #Leo el archivo CSV como un dataFrame
 
 for column in df.columns:
     df[column] = df[column].astype(str).apply(normalize)
