203b67b5ab139bb8f0ba3210a8355deb90d32449
AlvaroPaganoLIFIA <paganoalvaroangel@gmail.com>
2024-10-21 11:33:39 -0300

    probando patron multiof

--- extractor/src/rbm/patterns/multioferta.py
+++ extractor/src/rbm/patterns/multioferta.py
@@ -5,7 +5,7 @@ def multioferta() -> list:
         [{"POS": "NUM", "OP":"?"},{"LOWER": {"IN": ["lotes", "terrenos", "parcelas"]}},{"OP":"{1,2}"}, {"LOWER": {"IN": ["venta","medidas"]}}], #lotes en venta, lotes a la venta, lotes de diferentes medidas
         [{"LOWER": {"IN": ["lotes",  "terrenos"]}}, {"POS":"NUM"},{"LOWER": {"IN": ["x", "por"]}}], # lotes 10x30
         [{"LOWER": "juntos"}, {"LOWER": "o"}, {"LOWER": "separados"}], #se venden juntos o separados
-        [{"LOWER": "son"},{"LIKE_NUM": True},{"LOWER":"lotes."}],
+        [{"LOWER": "son"},{"LIKE_NUM": True},{"LOWER" : "lotes"}],# son 4 (numero) lotes
     ])
 
 def frases_multioferta_PM() -> list:
