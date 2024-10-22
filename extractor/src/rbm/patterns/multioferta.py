d5a1ae6756ef0faaea8b67fc14cdd6dd8af3b2c4
AlvaroPaganoLIFIA <paganoalvaroangel@gmail.com>
2024-10-22 14:27:52 -0300

    mejoras en multioferta

--- extractor/src/rbm/patterns/multioferta.py
+++ extractor/src/rbm/patterns/multioferta.py
@@ -4,10 +4,14 @@ def multioferta() -> list:
         [{"POS":"NUM"},{"LOWER": {"IN": ["lotes", "terrenos", "parcelas"]}}], #4 lotes
         [{"POS": "NUM", "OP":"?"},{"LOWER": {"IN": ["lotes", "terrenos", "parcelas"]}},{"OP":"{1,2}"}, {"LOWER": {"IN": ["venta","medidas"]}}], #lotes en venta, lotes a la venta, lotes de diferentes medidas
         [{"LOWER": {"IN": ["lotes",  "terrenos"]}}, {"POS":"NUM"},{"LOWER": {"IN": ["x", "por"]}}], # lotes 10x30
-        [{"LOWER": "juntos"}, {"LOWER": "o"}, {"LOWER": "separados"}], #se venden juntos o separados
+        [{"LOWER": "juntos"}, {"LOWER": "o"}, {"LOWER": "separados"}], 
         [{"LOWER": "son"},{"LIKE_NUM": True},{"LOWER" : "lotes"}],# son 4 (numero) lotes
+        [{"LOWER" : "lotes"},{"LOWER" : "de","OP":"?"},{"LIKE_NUM" : True},{"LOWER" : "m2"}], #lotes de (num) m2
+        [{"LOWER" : "barrio "},{"LOWER" : {"IN" :["abierto","cerrado"]},"OP":"?"},{"LOWER" : "con"},{"LIKE_NUM" : True},{"LOWER":"lotes"}]        
     ])
 
+# futura posible mejora con el phraseMatcher
+# frases = ["Barrio abierto con 150 lotes",""]
 def frases_multioferta_PM() -> list:
     frases = ["",""]
     return frases
