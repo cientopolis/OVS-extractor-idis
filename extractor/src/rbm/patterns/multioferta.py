584bda560112fb8407c4476873634d41d5cc7d02
AlvaroPaganoLIFIA <paganoalvaroangel@gmail.com>
2024-11-05 15:35:45 -0300

    cambios en multioferta.py,merge de test_branch en feat_multioferta

--- extractor/src/rbm/patterns/multioferta.py
+++ extractor/src/rbm/patterns/multioferta.py
@@ -1,19 +1,41 @@
+sinonimos_inmuebles = ["terrenos","lotes","casas","inmuebles","propiedades"]
 def multioferta() -> list:
     return([
         [{"LOWER": {"IN": ["cada", "varios", "multiples", "múltiples"]}}, {"LOWER": {"IN": ["lotes", "lote", "terernos", "terreno", "parcela", "parcelas"]}}], #cada lote, varios lotes, multiples lotes
         [{"POS":"NUM"},{"LOWER": {"IN": ["lotes", "terrenos", "parcelas"]}}], #4 lotes
         [{"POS": "NUM", "OP":"?"},{"LOWER": {"IN": ["lotes", "terrenos", "parcelas"]}},{"OP":"{1,2}"}, {"LOWER": {"IN": ["venta","medidas"]}}], #lotes en venta, lotes a la venta, lotes de diferentes medidas
         [{"LOWER": {"IN": ["lotes",  "terrenos"]}}, {"POS":"NUM"},{"LOWER": {"IN": ["x", "por"]}}], # lotes 10x30
+<<<<<<< HEAD
         [{"LOWER": "juntos"}, {"LOWER": "o"}, {"LOWER": "separados"}], 
         [{"LOWER": "son"},{"LIKE_NUM": True},{"LOWER" : "lotes"}],# son 4 (numero) lotes
         [{"LOWER" : "lotes"},{"LOWER" : "de","OP":"?"},{"LIKE_NUM" : True},{"LOWER" : "m2"}], #lotes de (num) m2
         [{"LOWER" : "barrio "},{"LOWER" : {"IN" :["abierto","cerrado"]},"OP":"?"},{"LOWER" : "con"},{"LIKE_NUM" : True},{"LOWER":"lotes"}]        
+=======
+        [{"LOWER": "juntos"}, {"LOWER": "o"}, {"LOWER": "separados"}], #se venden juntos o separados
+        # POSIBLES MEJORAS
+        [{"LOWER": "son"},{"LIKE_NUM": True},{"LOWER" : "lotes"}],# son 4 (numero) lotes
+        [{"LOWER" : "compuesto"},{"LOWER" : "por"},{"LIKE_NUM" : True},{"LOWER" : "unidades"},{"LOWER" : "funcionales"}], # compuesto por (num) unidades funcionales
+        [{"LOWER" : "venta"},{"LIKE_NUM" : True},{"LOWER" : "de","OP" : "?"},{"LOWER" : "lotes"}],# venta {de(op)} (num) lotes
+        #[{"LOWER" : "venta"},{"LOWER" : "de","OP" : "?"},{"LOWER" : {"IN" : [sinonimos_inmuebles]}}],# venta {de (op)} sinonimos_inmuebles 
+        [{"LOWER" : "venta"},{"LOWER" : "en","OP" : "?"},{"LOWER" : "block"}], # venta {en (op)} block
+        [{"LOWER" : "son"},{"LIKE_NUM" : True},{"LOWER" : "lotes"}], #son (num) lotes
+        [{"LOWER" : "venta"},{"LOWER" : "lotes"}], #venta lotes
+        [{"LOWER" : "lotes"},{"LOWER" : "de","OP":"?"},{"LIKE_NUM" : True}], # lotes de (num)
+
+>>>>>>> test_branch
     ])
 
 # futura posible mejora con el phraseMatcher
 # frases = ["Barrio abierto con 150 lotes",""]
 def frases_multioferta_PM() -> list:
-    frases = ["",""]
+    frases = ["Disponemos a la venta amplios lotes","disponemos a la venta amplios terrenos","disponemos a la venta inmbuebles",
+              "Medidas de los lotes","Medidas de los terrenos","medidas de los lotes","Lotes en PH",
+              "El inmueble consta de tres lotes","terreno consta de tres lotes","La propiedad consta de tres lotes","El inmueble consta de cuatro lotes",
+              "El inmueble consta de dos lotes","El inmueble consta de cinco lotes","dos lotes","tres lotes","cuatro lotes","cinco lotes",
+              "Opción de sumar otro lote lindero","Opción de sumar otro lindero","venta de unidades funcionales","Venta de terrenos","venta de lotes","VENTA EN BLOCK",
+              "Disponemos a la venta amplios lotes","Medidas de los lotes","LOTES en","posibilidad de venderse por separado o en bloque",
+              "El inmueble consta de tres lotes","se puede comprar lotes contiguos","DOS LOTES EN BLOQUE","para la venta Lotes"
+              ]
     return frases
 
     
