fcaf76ce4763ad9cfae4a30e2d2507aa87468f10
AlvaroPaganoLIFIA <paganoalvaroangel@gmail.com>
2024-11-05 16:29:52 -0300

    mejoras en patron multioferta

--- extractor/src/rbm/patterns/multioferta.py
+++ extractor/src/rbm/patterns/multioferta.py
@@ -7,7 +7,7 @@ def multioferta() -> list:
         [{"LOWER": {"IN": ["lotes",  "terrenos"]}}, {"POS":"NUM"},{"LOWER": {"IN": ["x", "por"]}}], # lotes 10x30
         [{"LOWER": "son"},{"LIKE_NUM": True},{"LOWER" : "lotes"}],# son 4 (numero) lotes
         [{"LOWER" : "lotes"},{"LOWER" : "de","OP":"?"},{"LIKE_NUM" : True},{"LOWER" : "m2"}], #lotes de (num) m2
-        [{"LOWER" : "barrio "},{"LOWER" : {"IN" :["abierto","cerrado"]},"OP":"?"},{"LOWER" : "con"},{"LIKE_NUM" : True},{"LOWER":"lotes"}], 
+        [{"LOWER" : "barrio "},{"LOWER" : {"IN" :["abierto","cerrado"]},"OP":"?"},{"LOWER" : "con"},{"LIKE_NUM" : True},{"LOWER":"lotes"}], # barrio abierto con 150 lotes # ANDA EN RAMA DE PRUEBA PERO NO ACA
         [{"LOWER": "juntos"}, {"LOWER": "o"}, {"LOWER": "separados"}], #se venden juntos o separados
         # POSIBLES MEJORAS
         [{"LOWER": "son"},{"LIKE_NUM": True},{"LOWER" : "lotes"}],# son 4 (numero) lotes
@@ -15,15 +15,13 @@ def multioferta() -> list:
         [{"LOWER" : "venta"},{"LIKE_NUM" : True},{"LOWER" : "de","OP" : "?"},{"LOWER" : "lotes"}],# venta {de(op)} (num) lotes
         #[{"LOWER" : "venta"},{"LOWER" : "de","OP" : "?"},{"LOWER" : {"IN" : [sinonimos_inmuebles]}}],# venta {de (op)} sinonimos_inmuebles 
         [{"LOWER" : "venta"},{"LOWER" : "en","OP" : "?"},{"LOWER" : "block"}], # venta {en (op)} block
-        [{"LOWER" : "son"},{"LIKE_NUM" : True},{"LOWER" : "lotes"}], #son (num) lotes
+        [{"LOWER" : "son"},{"LIKE_NUM" : True},{"LOWER" : "lotes"}], #son (num) lotes  # ANDA EN ARCHIVO DE PRUEBA PERO NO ACA
         [{"LOWER" : "venta"},{"LOWER" : "lotes"}], #venta lotes
-        [{"LOWER" : "lotes"},{"LOWER" : "de","OP":"?"},{"LIKE_NUM" : True}], # lotes de (num)
+        [{"LOWER" : "lotes"},{"LOWER" : "de","OP":"?"},{"LIKE_NUM" : True}], # lotes de (num)  #ANDA EN RAMA DE PRUEBA PERO NO ACA
         [{"LOWER": "con"},{"LIKE_NUM" : True},{"LOWER":"locales"}],#con 4 locales
-        [{"LOWER":"venta"},{"LIKE_NUM": True},{"LOWER": "lotes"}],#venta 80 lotes
+        [{"LOWER":"venta"},{"LIKE_NUM": True},{"LOWER": "Lotes"}],#venta 80 lotes # ANDA EN RAMA DE PRUEBA PERO NO ACA 
     ])
 
-# futura posible mejora con el phraseMatcher
-# frases = ["Barrio abierto con 150 lotes",""]
 def frases_multioferta_PM() -> list:
     frases = ["Disponemos a la venta amplios lotes","disponemos a la venta amplios terrenos","disponemos a la venta inmbuebles",
               "Medidas de los lotes","Medidas de los terrenos","medidas de los lotes","Lotes en PH",
@@ -32,8 +30,8 @@ def frases_multioferta_PM() -> list:
               "Opción de sumar otro lote lindero","Opción de sumar otro lindero","venta de unidades funcionales","Venta de terrenos","venta de lotes","VENTA EN BLOCK",
               "Disponemos a la venta amplios lotes","Medidas de los lotes","LOTES en","posibilidad de venderse por separado o en bloque",
               "El inmueble consta de tres lotes","se puede comprar lotes contiguos","DOS LOTES EN BLOQUE","para la venta Lotes","ideal para desarrollos multifamiliares",
-              "ideal desarrollos multifamiliares"
-              ]
+              "ideal desarrollos multifamiliares","venta 80 lotes","barrio abierto con 150 lotes","son 4 lotes"
+              ]#Las ultimas 3 frases la idea es hacerlas con el Matcher y no con el PM, se deja asi porque andan con el PM pero no con el M
     return frases
 
     
